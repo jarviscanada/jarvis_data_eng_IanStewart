0d2efe12c17bc5de901dbbfbcea4a939d726a31f
Ian Stewart <ianstewart4@outlook.com>
2024-01-24 15:53:08 +0000

    removed unnecessary variable

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -22,7 +22,6 @@ cpu_mhz=$(echo "$lscpu_out"  | egrep "^CPU MHz:" | awk '{print $3}' | xargs)
 l2_cache=$(echo "$lscpu_out" | egrep "^L2 cache:" | awk '{print $3}' | cut -c -3 | xargs)
 total_mem=$(echo "$vmstat_mb" | tail -1 | awk '{print $4}')
 timestamp=$(echo "$vmstat_mb" | tail -1 | awk '{print $18, $19}')
-host_id="(SELECT id FROM host_info WHERE hostname='$hostname')";
 insert_stmt="INSERT INTO host_info( hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, timestamp, total_mem) VALUES('$hostname', $cpu_number, '$cpu_architecture', '$cpu_model', '$cpu_mhz', '$l2_cache', '$timestamp', '$total_mem')"
 
 export PGPASSWORD=$psql_password 
