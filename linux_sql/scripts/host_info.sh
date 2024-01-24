ad67f8fa83cc18315aa0d9181d235291c48cac13
Ian Stewart <ianstewart4@outlook.com>
2024-01-24 15:27:16 +0000

    completed bash scripts

---
+++ linux_sql/scripts/host_info.sh
@@ -0,0 +1,45 @@
+# Setup and validate arguments (again, don't copy comments)
+psql_host=$1
+psql_port=$2
+db_name=$3
+psql_user=$4
+psql_password=$5
+
+# Check # of args
+if [ "$#" -ne 5 ]; then
+    echo "Illegal number of parameters"
+    exit 1
+fi
+
+# VARIABLES
+vmstat_mb=$(vmstat --unit M -t)
+hostname=$(hostname -f)
+
+#---------------------------------------------------------------------
+lscpu_out=`lscpu`
+cpu_number=$(echo "$lscpu_out"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
+
+# HARDWARE INFO
+hostname=$(hostname -f)
+cpu_number=$(echo "$lscpu_out"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
+cpu_architecture=$(echo "$lscpu_out"  | egrep "^Architecture:" | awk '{print $2}' | xargs)
+cpu_model=$(echo "$lscpu_out"  | egrep "^Model:" | awk '{print $2}' | xargs)
+cpu_mhz=$(echo "$lscpu_out"  | egrep "^CPU MHz:" | awk '{print $3}' | xargs)
+l2_cache=$(echo "$lscpu_out" | egrep "^L2 cache:" | awk '{print $3}' | cut -c -3 | xargs)
+total_mem=$(echo "$vmstat_mb" | tail -1 | awk '{print $4}')
+timestamp=$(echo "$vmstat_mb" | tail -1 | awk '{print $18, $19}') # current timestamp in `2019-11-26 14:40:19`
+
+#---------------------------------------------------------------------
+
+# Subquery to find matching id in host_info table
+host_id="(SELECT id FROM host_info WHERE hostname='$hostname')";
+
+# PSQL command: Inserts server usage data into host_usage table
+# Note: be careful with double and single quotes
+insert_stmt="INSERT INTO host_info( hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, timestamp, total_mem) VALUES('$hostname', $cpu_number, '$cpu_architecture', '$cpu_model', '$cpu_mhz', '$l2_cache', '$timestamp', '$total_mem')"
+
+#set up env var for pql cmd
+export PGPASSWORD=$psql_password 
+#Insert date into a database
+psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
+exit $?
